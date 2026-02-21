779937078ac42e37b49ae0d1e7cddce2ead0c7e0
Eike Mueller <eike.h.mueller@gmail.com>
2026-02-21 15:42:48 +0000

    Move shared code into separate function

--- cuda/src/derivatives.cu
+++ cuda/src/derivatives.cu
@@ -1,6 +1,52 @@
 #include "derivatives.hh"
 /** @brief implementation of derivatives.hh */
 
+__device__ void copy_patch_into_shared_memory(float *u, float *u_shared,
+                                              const GridSpec grid_spec)
+{
+  int nx = grid_spec.nx;
+  int ny = grid_spec.ny;
+  int nz = grid_spec.nz;
+  int a = blockDim.x * blockIdx.x + threadIdx.x;
+  int b = blockDim.y * blockIdx.y + threadIdx.y;
+  int c = blockDim.z * blockIdx.z + threadIdx.z;
+  if ((a < nx) && (b < ny) && (c < nz))
+  {
+    // Copy into shared memory
+    u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x + 1,
+                 threadIdx.y + 1, threadIdx.z + 1)] = u[IDX(nx, ny, nz, a, b, c)];
+    if (threadIdx.x == 0)
+      u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x,
+                   threadIdx.y + 1, threadIdx.z + 1)] =
+          u[IDX(nx, ny, nz, (a - 1 + nx) % nx, b, c)];
+    if (threadIdx.y == 0)
+      u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x + 1,
+                   threadIdx.y, threadIdx.z + 1)] =
+          u[IDX(nx, ny, nz, a, (b - 1 + ny) % ny, c)];
+    if (threadIdx.z == 0)
+      u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x + 1,
+                   threadIdx.y + 1, threadIdx.z)] =
+          u[IDX(nx, ny, nz, a, b, (c - 1 + nz) % nz)];
+    if ((threadIdx.x == 0) && (threadIdx.y == 0))
+      u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x,
+                   threadIdx.y, threadIdx.z + 1)] =
+          u[IDX(nx, ny, nz, (a - 1 + nx) % nx, (b - 1 + ny) % ny, c)];
+    if ((threadIdx.y == 0) && (threadIdx.z == 0))
+      u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x + 1,
+                   threadIdx.y, threadIdx.z)] =
+          u[IDX(nx, ny, nz, a, (b - 1 + ny) % ny, (c - 1 + nz) % nz)];
+    if ((threadIdx.z == 0) && (threadIdx.x == 0))
+      u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x,
+                   threadIdx.y + 1, threadIdx.z)] =
+          u[IDX(nx, ny, nz, (a - 1 + nx) % nx, b, (c - 1 + nz) % nz)];
+    if ((threadIdx.x == 0) && (threadIdx.y == 0) && (threadIdx.z == 0))
+      u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x,
+                   threadIdx.y, threadIdx.z)] =
+          u[IDX(nx, ny, nz, (a - 1 + nx) % nx, (b - 1 + ny) % ny,
+                (c - 1 + nz) % nz)];
+  }
+}
+
 /* Kernel for backward derivative computation in x-direction */
 __global__ void backward_derivative_x_kernel(float *u, float *du_dx,
                                              const GridSpec grid_spec)
@@ -10,66 +56,32 @@ __global__ void backward_derivative_x_kernel(float *u, float *du_dx,
   int ny = grid_spec.ny;
   int nz = grid_spec.nz;
   __shared__ float
-      _u[(BLOCKSIZE_X + 1) * (BLOCKSIZE_Y + 1) * (BLOCKSIZE_Z + 1)];
+      u_shared[(BLOCKSIZE_X + 1) * (BLOCKSIZE_Y + 1) * (BLOCKSIZE_Z + 1)];
   int a = blockDim.x * blockIdx.x + threadIdx.x;
   int b = blockDim.y * blockIdx.y + threadIdx.y;
   int c = blockDim.z * blockIdx.z + threadIdx.z;
-  if ((a < nx) && (b < ny) && (c < nz))
-  {
-    // Copy into shared memory
-    _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x + 1,
-           threadIdx.y + 1, threadIdx.z + 1)] = u[IDX(nx, ny, nz, a, b, c)];
-    if (threadIdx.x == 0)
-      _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x,
-             threadIdx.y + 1, threadIdx.z + 1)] =
-          u[IDX(nx, ny, nz, (a - 1 + nx) % nx, b, c)];
-    if (threadIdx.y == 0)
-      _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x + 1,
-             threadIdx.y, threadIdx.z + 1)] =
-          u[IDX(nx, ny, nz, a, (b - 1 + ny) % ny, c)];
-    if (threadIdx.z == 0)
-      _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x + 1,
-             threadIdx.y + 1, threadIdx.z)] =
-          u[IDX(nx, ny, nz, a, b, (c - 1 + nz) % nz)];
-    if ((threadIdx.x == 0) && (threadIdx.y == 0))
-      _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x,
-             threadIdx.y, threadIdx.z + 1)] =
-          u[IDX(nx, ny, nz, (a - 1 + nx) % nx, (b - 1 + ny) % ny, c)];
-    if ((threadIdx.y == 0) && (threadIdx.z == 0))
-      _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x + 1,
-             threadIdx.y, threadIdx.z)] =
-          u[IDX(nx, ny, nz, a, (b - 1 + ny) % ny, (c - 1 + nz) % nz)];
-    if ((threadIdx.z == 0) && (threadIdx.x == 0))
-      _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x,
-             threadIdx.y + 1, threadIdx.z)] =
-          u[IDX(nx, ny, nz, (a - 1 + nx) % nx, b, (c - 1 + nz) % nz)];
-    if ((threadIdx.x == 0) && (threadIdx.y == 0) && (threadIdx.z == 0))
-      _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1, threadIdx.x,
-             threadIdx.y, threadIdx.z)] =
-          u[IDX(nx, ny, nz, (a - 1 + nx) % nx, (b - 1 + ny) % ny,
-                (c - 1 + nz) % nz)];
-  }
+  copy_patch_into_shared_memory(u, u_shared, grid_spec);
   __syncthreads();
   if ((a < nx) && (b < ny) && (c < nz))
   {
     float _du_dx = 0;
     // apply derivative
-    _du_dx += _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
-                     threadIdx.x + 1, threadIdx.y + 1, threadIdx.z + 1)];
-    _du_dx += _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
-                     threadIdx.x + 1, threadIdx.y, threadIdx.z + 1)];
-    _du_dx += _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
-                     threadIdx.x + 1, threadIdx.y + 1, threadIdx.z)];
-    _du_dx += _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
-                     threadIdx.x + 1, threadIdx.y, threadIdx.z)];
-    _du_dx -= _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
-                     threadIdx.x, threadIdx.y + 1, threadIdx.z + 1)];
-    _du_dx -= _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
-                     threadIdx.x, threadIdx.y, threadIdx.z + 1)];
-    _du_dx -= _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
-                     threadIdx.x, threadIdx.y + 1, threadIdx.z)];
-    _du_dx -= _u[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
-                     threadIdx.x, threadIdx.y, threadIdx.z)];
+    _du_dx += u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
+                           threadIdx.x + 1, threadIdx.y + 1, threadIdx.z + 1)];
+    _du_dx += u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
+                           threadIdx.x + 1, threadIdx.y, threadIdx.z + 1)];
+    _du_dx += u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
+                           threadIdx.x + 1, threadIdx.y + 1, threadIdx.z)];
+    _du_dx += u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
+                           threadIdx.x + 1, threadIdx.y, threadIdx.z)];
+    _du_dx -= u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
+                           threadIdx.x, threadIdx.y + 1, threadIdx.z + 1)];
+    _du_dx -= u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
+                           threadIdx.x, threadIdx.y, threadIdx.z + 1)];
+    _du_dx -= u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
+                           threadIdx.x, threadIdx.y + 1, threadIdx.z)];
+    _du_dx -= u_shared[IDX(BLOCKSIZE_X + 1, BLOCKSIZE_Y + 1, BLOCKSIZE_Z + 1,
+                           threadIdx.x, threadIdx.y, threadIdx.z)];
     //  Copy back to global memory
     du_dx[IDX(nx, ny, nz, a, b, c)] = 0.25 * h_inv * _du_dx;
   }
