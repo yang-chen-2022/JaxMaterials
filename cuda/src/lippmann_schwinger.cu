e01d81e84171fb779285410c01c6fe9fbf34cc04
Eike Mueller <eike.h.mueller@gmail.com>
2026-02-23 10:58:28 +0000

    Added method for computing stress from strain

--- cuda/src/lippmann_schwinger.cu
+++ cuda/src/lippmann_schwinger.cu
@@ -1,5 +1,42 @@
 #include "lippmann_schwinger.hh"
 
+/* Kernel for computing stress sigma_{ij} = C_{ijkl} epsilon_{kl} with
+ *
+ *     C_{ijkl} = lambda*delta_{ij}delta_{kl} + mu*(delta_{ik}delta_{jl}+delta_{il}delta_{jk})
+ */
+__global__ void compute_stress_kernel(float *dev_epsilon, float *dev_sigma,
+                                      float *dev_lambda, float *dev_mu,
+                                      const int ncells)
+{
+  int ell = blockDim.x * blockIdx.x + threadIdx.x;
+  float tr_epsilon;
+  if (ell < ncells)
+  {
+    tr_epsilon = dev_epsilon[ell] + dev_epsilon[ncells + ell] + dev_epsilon[2 * ncells + ell];
+    for (int mu = 0; mu < 3; ++mu)
+    {
+      int idx = mu * ncells + ell;
+      dev_sigma[idx] = 2 * dev_mu[idx] * dev_epsilon[idx] + dev_lambda[idx] * tr_epsilon;
+    }
+    for (int mu = 3; mu < 6; ++mu)
+    {
+      int idx = mu * ncells + ell;
+      dev_sigma[idx] = 2 * dev_mu[idx] * dev_epsilon[idx];
+    }
+  }
+}
+
+/* Compute stress sigma_{ij} = C_{ijkl} epsilon_{kl} on device */
+void compute_stress(float *dev_epsilon, float *dev_sigma,
+                    float *dev_lambda, float *dev_mu,
+                    const GridSpec grid_spec)
+{
+  int ncells = grid_spec.number_of_cells();
+  const int nblocks = (ncells + BLOCKSIZE - 1) / BLOCKSIZE;
+  compute_stress_kernel<<<nblocks, BLOCKSIZE>>>(dev_epsilon, dev_sigma,
+                                                dev_lambda, dev_mu, ncells);
+}
+
 void lippmann_schwinger_solve(const GridSpec grid_spec)
 {
   // halo size
--- cuda/src/lippmann_schwinger.hh
+++ cuda/src/lippmann_schwinger.hh
@@ -5,6 +5,23 @@
 #include "derivatives.hh"
 #include "fourier_solve.hh"
 
+/** @brief Compute stress on device
+ *
+ * Given epsilon and spatially varying Lame parameters lambda, mu compute the
+ * stress sigma according to sigma_{ij} = C_{ijkl} epsilon_{kl} where
+ *
+ *      C_{ijkl} = lambda*delta_{ij}delta_{kl} + mu*(delta_{ik}delta_{jl}+delta_{il}delta_{jk})
+ *
+ * @param[in] dev_epsilon strain epsilon in real space (device array, size 6*ncells)
+ * @param[out] dev_sigma resulting stress sigma in real space (device array, size 6*ncells)
+ * @param[in] dev_lambda Lame parameter lambda (device array, size 6*ncells)
+ * @param[in] dev_mu Lame parameter mu (device array, size 6*ncells)
+ * @param[in] grid_spec Specification of computational grid
+ */
+void compute_stress(float *dev_epsilon, float *dev_sigma,
+                    float *dev_lambda, float *dev_mu,
+                    const GridSpec grid_spec);
+
 void lippmann_schwinger_solve(const GridSpec grid_spec);
 
 #endif // LIPPMANN_SCHWINGER_HH
