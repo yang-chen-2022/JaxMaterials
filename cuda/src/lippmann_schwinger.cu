82a5afb81393fa273eade2ffa1af04ba35430374
Eike Mueller <eike.h.mueller@gmail.com>
2026-02-26 17:54:12 +0000

    Major refactoring: create dedicated class for Lippmann Schwinger solver. This allows separating initialisation (allocation), solver and destruction (deallocation) into separate functions. It will also allow calling the solver multiple times for the same setup without incurring setup costs every time.

--- cuda/src/lippmann_schwinger.cu
+++ cuda/src/lippmann_schwinger.cu
@@ -1,5 +1,7 @@
 #include "lippmann_schwinger.hh"
 
+/* **** CUDA kernels **** */
+
 /* Kernel for setting the values of epsilon to the constantbar(epsilon) */
 __global__ void set_epsilon_bar_kernel(float *dev_epsilon, float *epsilon_bar, const int ncells)
 {
@@ -7,15 +9,6 @@ __global__ void set_epsilon_bar_kernel(float *dev_epsilon, float *epsilon_bar, c
   if (ell < ncells)
     for (int alpha = 0; alpha < 6; ++alpha)
       dev_epsilon[alpha * ncells + ell] = alpha;
-}
-
-/* Set the values of epsilon to bar(epsilon) on the device */
-void set_epsilon_bar(float *dev_epsilon, float *epsilon_bar,
-                     const GridSpec grid_spec)
-{
-  int ncells = grid_spec.number_of_cells();
-  const int nblocks = (ncells + BLOCKSIZE - 1) / BLOCKSIZE;
-  set_epsilon_bar_kernel<<<nblocks, BLOCKSIZE>>>(dev_epsilon, epsilon_bar, ncells);
 }
 
 /* Kernel for computing stress sigma_{ij} = C_{ijkl} epsilon_{kl} with
@@ -45,17 +38,6 @@ __global__ void compute_stress_kernel(float *dev_epsilon, float *dev_sigma,
   }
 }
 
-/* Compute stress sigma_{ij} = C_{ijkl} epsilon_{kl} on device */
-void compute_stress(float *dev_epsilon, float *dev_sigma,
-                    float *dev_lambda, float *dev_mu,
-                    const GridSpec grid_spec)
-{
-  int ncells = grid_spec.number_of_cells();
-  const int nblocks = (ncells + BLOCKSIZE - 1) / BLOCKSIZE;
-  compute_stress_kernel<<<nblocks, BLOCKSIZE>>>(dev_epsilon, dev_sigma,
-                                                dev_lambda, dev_mu, ncells);
-}
-
 /* Kernel for incrementing solution epsilon -> epsilon + alpha*r */
 __global__ void increment_solution_kernel(float *dev_epsilon, cufftComplex *dev_r,
                                           const float alpha,
@@ -66,17 +48,6 @@ __global__ void increment_solution_kernel(float *dev_epsilon, cufftComplex *dev_
   {
     dev_epsilon[ell] += alpha * dev_r[ell].x;
   }
-}
-
-/* Increment solution epsilon -> epsilon + 1/ncells * r */
-void increment_solution(float *dev_epsilon, cufftComplex *dev_r,
-                        const GridSpec grid_spec)
-{
-  int ncells = grid_spec.number_of_cells();
-  int ndof = 6 * ncells;
-  const int nblocks = (ndof + BLOCKSIZE - 1) / BLOCKSIZE;
-  float alpha = 1.0 / float(ncells);
-  increment_solution_kernel<<<nblocks, BLOCKSIZE>>>(dev_epsilon, dev_r, alpha, ndof);
 }
 
 /* Kernel for computing stress divergence in Fourier space */
@@ -105,9 +76,39 @@ __global__ void divergence_fourier_kernel(cufftComplex *dev_sigma_hat, float *de
   }
 }
 
+/* **** class methods **** */
+
+/* Set the values of epsilon to bar(epsilon) on the device */
+void LippmannSchwingerSolver::set_epsilon_bar(float *dev_epsilon, float *epsilon_bar)
+{
+  int ncells = grid_spec.number_of_cells();
+  const int nblocks = (ncells + BLOCKSIZE - 1) / BLOCKSIZE;
+  set_epsilon_bar_kernel<<<nblocks, BLOCKSIZE>>>(dev_epsilon, epsilon_bar, ncells);
+}
+
+/* Compute stress sigma_{ij} = C_{ijkl} epsilon_{kl} on device */
+void LippmannSchwingerSolver::compute_stress(float *dev_epsilon, float *dev_sigma,
+                                             float *dev_lambda, float *dev_mu)
+{
+  int ncells = grid_spec.number_of_cells();
+  const int nblocks = (ncells + BLOCKSIZE - 1) / BLOCKSIZE;
+  compute_stress_kernel<<<nblocks, BLOCKSIZE>>>(dev_epsilon, dev_sigma,
+                                                dev_lambda, dev_mu, ncells);
+}
+
+/* Increment solution epsilon -> epsilon + 1/ncells * r */
+void LippmannSchwingerSolver::increment_solution(float *dev_epsilon, cufftComplex *dev_r)
+{
+  int ncells = grid_spec.number_of_cells();
+  int ndof = 6 * ncells;
+  const int nblocks = (ndof + BLOCKSIZE - 1) / BLOCKSIZE;
+  float alpha = 1.0 / float(ncells);
+  increment_solution_kernel<<<nblocks, BLOCKSIZE>>>(dev_epsilon, dev_r, alpha, ndof);
+}
+
 /* compute divergence in Fourier space */
-void divergence_fourier(cufftComplex *dev_sigma_hat, float *dev_xi, cufftComplex *dev_div_sigma_hat,
-                        const GridSpec grid_spec)
+void LippmannSchwingerSolver::divergence_fourier(cufftComplex *dev_sigma_hat,
+                                                 cufftComplex *dev_div_sigma_hat)
 {
   int ncells = grid_spec.number_of_cells();
   const int nblocks = (ncells + BLOCKSIZE - 1) / BLOCKSIZE;
@@ -115,8 +116,8 @@ void divergence_fourier(cufftComplex *dev_sigma_hat, float *dev_xi, cufftComplex
 }
 
 /* Compute normalised divergence for stopping criterion */
-float relative_divergence_norm(cufftComplex *dev_sigma_hat, cufftComplex *dev_div_sigma_hat,
-                               cublasHandle_t &handle, const GridSpec grid_spec)
+float LippmannSchwingerSolver::relative_divergence_norm(cufftComplex *dev_sigma_hat,
+                                                        cufftComplex *dev_div_sigma_hat)
 {
   int ncells = grid_spec.number_of_cells();
   // STEP 1: Compute nrm_div_sigma =  <||div(sigma)||^2>
@@ -126,8 +127,6 @@ float relative_divergence_norm(cufftComplex *dev_sigma_hat, cufftComplex *dev_di
   // STEP 2: compute ||<sigma>||^2
   float nrm2_sigma = 0;
   // Extract zero mode, which is identical to the sum of sigma over the domain, i.e. ncells * <sigma>
-  cufftComplex *sigma_0;
-  CUDA_CHECK(cudaMallocHost(&sigma_0, 6 * sizeof(cufftComplex)));
   CUDA_CHECK(cudaMemcpy2D(sigma_0, sizeof(cufftComplex), dev_sigma_hat, ncells * sizeof(cufftComplex), sizeof(cufftComplex), 6, cudaMemcpyDeviceToHost));
   // Normalise by number of cells
   for (int alpha = 0; alpha < 6; ++alpha)
@@ -138,10 +137,107 @@ float relative_divergence_norm(cufftComplex *dev_sigma_hat, cufftComplex *dev_di
   // Compute norm of zero mode
   for (int alpha = 0; alpha < 6; ++alpha)
     nrm2_sigma += (1 + (2 < alpha)) * (sigma_0[alpha].x * sigma_0[alpha].x + sigma_0[alpha].y * sigma_0[alpha].y);
-  CUDA_CHECK(cudaFreeHost(sigma_0));
   return sqrt(nrm2_div_sigma / nrm2_sigma);
 }
 
+/* Constructor */
+LippmannSchwingerSolver::LippmannSchwingerSolver(const GridSpec grid_spec) : grid_spec(grid_spec)
+{
+  int ncells = grid_spec.number_of_cells();
+  // Initialise cuBLAS
+  CUBLAS_CHECK(cublasCreate(&handle));
+  // Set up cuFFT plan
+  int n[3] = {grid_spec.nz, grid_spec.ny, grid_spec.nx};
+  CUFFT_CHECK(cufftPlanMany(&plan, 3, n, n, 1, ncells, n, 1, ncells, CUFFT_C2C, 6));
+  CUDA_CHECK(cudaMalloc(&dev_xi_zero, 3 * ncells * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&dev_xi, 3 * ncells * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&dev_lambda, 6 * ncells * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&dev_mu, 6 * ncells * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&dev_epsilon, 6 * ncells * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&dev_sigma, 6 * ncells * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&dev_div_sigma, 3 * ncells * sizeof(float)));
+  CUDA_CHECK(cudaMalloc(&dev_div_sigma_hat, 3 * ncells * sizeof(cufftComplex)));
+  CUDA_CHECK(cudaMalloc(&dev_sigma_hat, 6 * ncells * sizeof(cufftComplex)));
+  CUDA_CHECK(cudaMalloc(&dev_residual, 6 * ncells * sizeof(cufftComplex)));
+  CUDA_CHECK(cudaMallocHost(&sigma_0, 6 * sizeof(cufftComplex)));
+  // initialize Fourier vectors
+  initialize_xi(dev_xi, grid_spec);
+  initialize_xizero(dev_xi_zero, grid_spec);
+  CUDA_CHECK(cudaDeviceSynchronize());
+}
+
+/* Destructor */
+LippmannSchwingerSolver::~LippmannSchwingerSolver()
+{
+  // free memory
+  CUDA_CHECK(cudaFree(dev_xi));
+  CUDA_CHECK(cudaFree(dev_xi_zero));
+  CUDA_CHECK(cudaFree(dev_lambda));
+  CUDA_CHECK(cudaFree(dev_mu));
+  CUDA_CHECK(cudaFree(dev_epsilon));
+  CUDA_CHECK(cudaFree(dev_sigma));
+  CUDA_CHECK(cudaFree(dev_div_sigma));
+  CUDA_CHECK(cudaFree(dev_div_sigma_hat));
+  CUDA_CHECK(cudaFree(dev_residual));
+  CUDA_CHECK(cudaFree(dev_sigma_hat));
+  CUDA_CHECK(cudaFreeHost(sigma_0));
+  CUBLAS_CHECK(cublasDestroy(handle));
+}
+
+/* apply solver */
+int LippmannSchwingerSolver::apply(float *lambda, float *mu, float *epsilon_bar,
+                                   float *epsilon, float *sigma,
+                                   float tolerance, int maxiter)
+{
+  int ncells = grid_spec.number_of_cells();
+  // Average values of lambda and mu
+  float lambda_0 = 0.5 * (*std::max_element(lambda, lambda + ncells) + *std::min_element(lambda, lambda + ncells));
+  float mu_0 = 0.5 * (*std::min_element(lambda, lambda + ncells) + *std::min_element(mu, mu + ncells));
+
+  // copy Lame parameters to device
+  CUDA_CHECK(cudaMemcpy(dev_lambda, lambda, ncells * sizeof(float), cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(dev_mu, mu, ncells * sizeof(float), cudaMemcpyHostToDevice));
+
+  // set average value of epsilon
+  set_epsilon_bar(dev_epsilon, epsilon_bar);
+  CUDA_CHECK(cudaDeviceSynchronize());
+  // main Lippmann-Schwinger loop
+  float rel_div_norm = 0;
+  int iter;
+  for (iter = 0; iter < maxiter; ++iter)
+  {
+    printf("iteration %4d\n", iter);
+    /* ==== STEP 1 ==== Compute stress: sigma_{ij} = C_{ijkl} epsilon_{kl} */
+    compute_stress(dev_epsilon, dev_sigma, dev_lambda, dev_mu);
+    /* ==== STEP 2 ==== Fourier transform:  hat(sigma) = FFT(sigma)*/
+    CUDA_CHECK(cudaMemset(dev_sigma_hat, 0, 6 * ncells * sizeof(cufftComplex)));
+    CUDA_CHECK(cudaMemcpy2D(dev_sigma_hat, 2 * sizeof(float), dev_sigma, sizeof(float), sizeof(float), 6 * ncells, cudaMemcpyDeviceToDevice));
+    CUFFT_CHECK(cufftExecC2C(plan, dev_sigma_hat, dev_sigma_hat, CUFFT_FORWARD));
+    CUDA_CHECK(cudaDeviceSynchronize());
+    /* ==== STEP 3 ==== Check convergence */
+    // Compute divergence in Fourier space
+    divergence_fourier(dev_sigma_hat, dev_div_sigma_hat);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    rel_div_norm = relative_divergence_norm(dev_sigma_hat, dev_div_sigma_hat);
+    printf("rel divergence norm = %e\n", rel_div_norm);
+    if (rel_div_norm < tolerance)
+      break;
+    /* ==== STEP 4 ==== Solve in Fourier space: hat(r)_{kl} = -Gamma^{0}_{klij} hat(sigma)_{ij} */
+    fourier_solve_device(dev_sigma_hat, dev_residual, dev_xi_zero, lambda_0, mu_0, grid_spec);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    /* ==== STEP 5 ==== Inverse Fourier transform: r = FFT^{-1}(hat(r)) */
+    CUFFT_CHECK(cufftExecC2C(plan, dev_residual, dev_residual, CUFFT_INVERSE));
+    CUDA_CHECK(cudaDeviceSynchronize());
+    /* ==== STEP 6 ==== Update solution: epsilon -> epsilon + r*/
+    increment_solution(dev_epsilon, dev_residual);
+    CUDA_CHECK(cudaDeviceSynchronize());
+  }
+  // Copy solution back to host
+  CUDA_CHECK(cudaMemcpy(epsilon, dev_epsilon, 6 * ncells * sizeof(float), cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaMemcpy(sigma, dev_sigma, 6 * ncells * sizeof(float), cudaMemcpyDeviceToHost));
+
+  return iter;
+}
 /* Lippmann Schwinger iteration */
 extern "C"
 {
@@ -158,97 +254,10 @@ extern "C"
     grid_spec.Lx = extents[0];
     grid_spec.Ly = extents[1];
     grid_spec.Lz = extents[2];
-    cublasHandle_t handle;
-
-    int ncells = grid_spec.number_of_cells();
-    // Set up cuFFT plan
-    cufftHandle plan;
-    cufftComplex *data;
-    CUDA_CHECK(cudaMalloc(&data, 6 * ncells * sizeof(cufftComplex)));
-    int n[3] = {grid_spec.nz, grid_spec.ny, grid_spec.nx};
-    CUFFT_CHECK(cufftPlanMany(&plan, 3, n, n, 1, ncells, n, 1, ncells, CUFFT_C2C, 6));
-    // Average values of lambda and mu
-    float lambda_0 = 0.5 * (*std::max_element(lambda, lambda + ncells) + *std::min_element(lambda, lambda + ncells));
-    float mu_0 = 0.5 * (*std::min_element(lambda, lambda + ncells) + *std::min_element(mu, mu + ncells));
-    // allocate device memory
-    float *dev_xi = nullptr;                   // Fourier vectors
-    float *dev_xi_zero = nullptr;              // normalised Fourier vectors
-    float *dev_lambda = nullptr;               // Lame parameter lamba on device
-    float *dev_mu = nullptr;                   // Lame parameter mu on device
-    float *dev_epsilon = nullptr;              // real-valued strain epsilon on device
-    float *dev_sigma = nullptr;                // real-valued stress sigma on device
-    float *dev_div_sigma = nullptr;            // divergence of sigma on device
-    cufftComplex *dev_sigma_hat = nullptr;     // complex-valued Fourier-stress on device
-    cufftComplex *dev_div_sigma_hat = nullptr; // complex-valued divergence of Fourier-stress on device
-    cufftComplex *dev_residual = nullptr;      // complex-valued residual on device
-    CUDA_CHECK(cudaMalloc(&dev_xi_zero, 3 * ncells * sizeof(float)));
-    CUDA_CHECK(cudaMalloc(&dev_xi, 3 * ncells * sizeof(float)));
-    CUDA_CHECK(cudaMalloc(&dev_lambda, 6 * ncells * sizeof(float)));
-    CUDA_CHECK(cudaMalloc(&dev_mu, 6 * ncells * sizeof(float)));
-    CUDA_CHECK(cudaMalloc(&dev_epsilon, 6 * ncells * sizeof(float)));
-    CUDA_CHECK(cudaMalloc(&dev_sigma, 6 * ncells * sizeof(float)));
-    CUDA_CHECK(cudaMalloc(&dev_div_sigma, 3 * ncells * sizeof(float)));
-    CUDA_CHECK(cudaMalloc(&dev_div_sigma_hat, 3 * ncells * sizeof(cufftComplex)));
-    CUDA_CHECK(cudaMalloc(&dev_sigma_hat, 6 * ncells * sizeof(cufftComplex)));
-    CUDA_CHECK(cudaMalloc(&dev_residual, 6 * ncells * sizeof(cufftComplex)));
-    // copy Lame parameters to device
-    CUDA_CHECK(cudaMemcpy(dev_lambda, lambda, ncells * sizeof(float), cudaMemcpyHostToDevice));
-    CUDA_CHECK(cudaMemcpy(dev_mu, mu, ncells * sizeof(float), cudaMemcpyHostToDevice));
-    // set average value of epsilon
-    set_epsilon_bar(dev_epsilon, epsilon_bar, grid_spec);
-    CUDA_CHECK(cudaDeviceSynchronize());
-    // initialize Fourier vectors
-    initialize_xi(dev_xi, grid_spec);
-    initialize_xizero(dev_xi_zero, grid_spec);
-    CUDA_CHECK(cudaDeviceSynchronize());
-    CUBLAS_CHECK(cublasCreate(&handle));
-    // main Lippmann-Schwinger loop
-    float rel_div_norm = 0;
-    int iter;
-    for (iter = 0; iter < maxiter; ++iter)
-    {
-      printf("iteration %4d\n", iter);
-      /* ==== STEP 1 ==== Compute stress: sigma_{ij} = C_{ijkl} epsilon_{kl} */
-      compute_stress(dev_epsilon, dev_sigma, dev_lambda, dev_mu, grid_spec);
-      /* ==== STEP 2 ==== Fourier transform:  hat(sigma) = FFT(sigma)*/
-      CUDA_CHECK(cudaMemset(dev_sigma_hat, 0, 6 * ncells * sizeof(cufftComplex)));
-      CUDA_CHECK(cudaMemcpy2D(dev_sigma_hat, 2 * sizeof(float), dev_sigma, sizeof(float), sizeof(float), 6 * ncells, cudaMemcpyDeviceToDevice));
-      CUFFT_CHECK(cufftExecC2C(plan, dev_sigma_hat, dev_sigma_hat, CUFFT_FORWARD));
-      CUDA_CHECK(cudaDeviceSynchronize());
-      /* ==== STEP 3 ==== Check convergence */
-      // Compute divergence in Fourier space
-      divergence_fourier(dev_sigma_hat, dev_xi, dev_div_sigma_hat, grid_spec);
-      CUDA_CHECK(cudaDeviceSynchronize());
-      rel_div_norm = relative_divergence_norm(dev_sigma_hat, dev_div_sigma_hat, handle, grid_spec);
-      printf("rel divergence norm = %e\n", rel_div_norm);
-      if (rel_div_norm < tolerance)
-        break;
-      /* ==== STEP 4 ==== Solve in Fourier space: hat(r)_{kl} = -Gamma^{0}_{klij} hat(sigma)_{ij} */
-      fourier_solve_device(dev_sigma_hat, dev_residual, dev_xi_zero, lambda_0, mu_0, grid_spec);
-      CUDA_CHECK(cudaDeviceSynchronize());
-      /* ==== STEP 5 ==== Inverse Fourier transform: r = FFT^{-1}(hat(r)) */
-      CUFFT_CHECK(cufftExecC2C(plan, dev_residual, dev_residual, CUFFT_INVERSE));
-      CUDA_CHECK(cudaDeviceSynchronize());
-      /* ==== STEP 6 ==== Update solution: epsilon -> epsilon + r*/
-      increment_solution(dev_epsilon, dev_residual, grid_spec);
-      CUDA_CHECK(cudaDeviceSynchronize());
-    }
-    // Copy solution back to host
-    CUDA_CHECK(cudaMemcpy(epsilon, dev_epsilon, 6 * ncells * sizeof(float), cudaMemcpyDeviceToHost));
-    CUDA_CHECK(cudaMemcpy(sigma, dev_sigma, 6 * ncells * sizeof(float), cudaMemcpyDeviceToHost));
-
-    // free memory
-    CUDA_CHECK(cudaFree(dev_xi));
-    CUDA_CHECK(cudaFree(dev_xi_zero));
-    CUDA_CHECK(cudaFree(dev_lambda));
-    CUDA_CHECK(cudaFree(dev_mu));
-    CUDA_CHECK(cudaFree(dev_epsilon));
-    CUDA_CHECK(cudaFree(dev_sigma));
-    CUDA_CHECK(cudaFree(dev_div_sigma));
-    CUDA_CHECK(cudaFree(dev_div_sigma_hat));
-    CUDA_CHECK(cudaFree(dev_residual));
-    CUDA_CHECK(cudaFree(dev_sigma_hat));
-    CUBLAS_CHECK(cublasDestroy(handle));
+    LippmannSchwingerSolver solver(grid_spec);
+    int iter = solver.apply(lambda, mu, epsilon_bar,
+                            epsilon, sigma,
+                            tolerance, maxiter);
     return iter;
   }
 }
--- cuda/src/lippmann_schwinger.hh
+++ cuda/src/lippmann_schwinger.hh
@@ -8,59 +8,123 @@
 #include "derivatives.hh"
 #include "fourier_solve.hh"
 
-/** @brief Compute stress on device
- *
- * Given epsilon and spatially varying Lame parameters lambda, mu compute the
- * stress sigma according to sigma_{ij} = C_{ijkl} epsilon_{kl} where
- *
- *      C_{ijkl} = lambda*delta_{ij}delta_{kl} + mu*(delta_{ik}delta_{jl}+delta_{il}delta_{jk})
- *
- * @param[in] dev_epsilon strain epsilon in real space (device array, size 6*ncells)
- * @param[out] dev_sigma resulting stress sigma in real space (device array, size 6*ncells)
- * @param[in] dev_lambda Lame parameter lambda (device array, size ncells)
- * @param[in] dev_mu Lame parameter mu (device array, size ncells)
- * @param[in] grid_spec Specification of computational grid
- */
-void compute_stress(float *dev_epsilon, float *dev_sigma,
-                    float *dev_lambda, float *dev_mu,
-                    int *cells,
-                    float *extents);
+class LippmannSchwingerSolver
+{
+public:
+    /** @brief Constructor create new instance
+     *
+     * Initialise all state variables and allocate required memory
+     *
+     * @param[in] grid_spec specification of computational grid
+     */
+    LippmannSchwingerSolver(const GridSpec grid_spec);
 
-/** @brief Increment solution
- *
- * Increment
- *
- *      epsilon -> epsilon + 1/ncells * r
- *
- * where the factor 1/ncells arises since the inverse Fourier transformation
- * in cuFFT is not normalised.
- *
- * @param[inout] dev_epsilon solution (device array, size 6*ncells)
- * @param[in] dev_r increment (device array, size 6*ncells)
- * @param[in] grid_spec specification of computational grid
- */
-void increment_solution(float *dev_epsilon, cufftComplex *dev_r,
-                        const GridSpec grid_spec);
+    /** @brief Destructor
+     *
+     * Free all allocated memory
+     */
+    ~LippmannSchwingerSolver();
 
-/** @brief Compute normalised divergence for stopping criterion in Fourier space
- *
- * Compute the relative divergence norm
- *
- *      sqrt(<||div(sigma)||^2>) / ||<sigma>||
- *
- * Which in Fourier space is given by
- *
- *      sqrt(<||xi.hat(sigma)||^2>) / ||hat(sigma)(0)||
- *
- * @param[in] dev_sigma_hat stress in Fourier space
- * @param[in] dev_div_sigma_hat divergence of stress in Fourier space
- * @param[in] handle cuBLAS handle
- * @param[in] grid_spec specification of grid
- */
-float relative_divergence_norm(cufftComplex *dev_sigma_hat, cufftComplex *dev_div_sigma_hat,
-                               cublasHandle_t &handle, const GridSpec grid_spec);
+    /** @brief Compute stress on device
+     *
+     * Given epsilon and spatially varying Lame parameters lambda, mu compute the
+     * stress sigma according to sigma_{ij} = C_{ijkl} epsilon_{kl} where
+     *
+     *      C_{ijkl} = lambda*delta_{ij}delta_{kl} + mu*(delta_{ik}delta_{jl}+delta_{il}delta_{jk})
+     *
+     * @param[in] dev_epsilon strain epsilon in real space (device array, size 6*ncells)
+     * @param[out] dev_sigma resulting stress sigma in real space (device array, size 6*ncells)
+     * @param[in] dev_lambda Lame parameter lambda (device array, size ncells)
+     * @param[in] dev_mu Lame parameter mu (device array, size ncells)
+     */
+    void compute_stress(float *dev_epsilon, float *dev_sigma,
+                        float *dev_lambda, float *dev_mu);
+
+    /** @brief Solve for a given set of Lame parameters
+     *
+     * @param[in]
+     * @param[in] lambda Lame parameter lambda (host array, size ncells)
+     * @param[in] mu Lame parameter mu (host array, size ncells)
+     * @param[in] epsilon_bar average value of epsilon (host array, size 6)
+     * @param[out] epsilon Resulting strain (host array, size 6*ncells)
+     * @param[out] sigma Resulting stress (host array, size 6*ncells)
+     * @param[in] tolerance tolerance on normalised divergence
+     * @param[in] maxiter maximum number of iterations
+     */
+    int apply(float *lambda, float *mu, float *epsilon_bar,
+              float *epsilon, float *sigma,
+              float tolerance, int maxiter);
+
+protected:
+    /** @brief Increment solution
+     *
+     * Increment
+     *
+     *      epsilon -> epsilon + 1/ncells * r
+     *
+     * where the factor 1/ncells arises since the inverse Fourier transformation
+     * in cuFFT is not normalised.
+     *
+     * @param[inout] dev_epsilon solution (device array, size 6*ncells)
+     * @param[in] dev_increment increment (device array, size 6*ncells)
+     */
+    void increment_solution(float *dev_epsilon, cufftComplex *dev_increment);
+
+    /** @brief Compute normalised divergence for stopping criterion in Fourier space
+     *
+     * Compute the relative divergence norm
+     *
+     *      sqrt(<||div(sigma)||^2>) / ||<sigma>||
+     *
+     * Which in Fourier space is given by
+     *
+     *      sqrt(<||xi.hat(sigma)||^2>) / ||hat(sigma)(0)||
+     *
+     * @param[in] dev_sigma_hat stress in Fourier space
+     * @param[in] dev_div_sigma_hat divergence of stress in Fourier space
+     */
+    float relative_divergence_norm(cufftComplex *dev_sigma_hat, cufftComplex *dev_div_sigma_hat);
+
+    /* Set the values of epsilon to bar(epsilon) on the device */
+    void set_epsilon_bar(float *dev_epsilon, float *epsilon_bar);
+
+    /* compute divergence in Fourier space */
+    void divergence_fourier(cufftComplex *dev_sigma_hat, cufftComplex *dev_div_sigma_hat);
+
+    /* Class variables */
+    /** @brief specification of computational grid */
+    const GridSpec grid_spec;
+    /** @brief Fourier vectors */
+    float *dev_xi;
+    /** @brief normalised Fourier vectors */
+    float *dev_xi_zero;
+    /** @brief Lame parameter lamba on device */
+    float *dev_lambda;
+    /** @brief Lame parameter mu on device */
+    float *dev_mu;
+    /** @brief real-valued strain epsilon on device */
+    float *dev_epsilon;
+    /** @brief real-valued stress sigma on device */
+    float *dev_sigma;
+    /** @brief divergence of sigma on device */
+    float *dev_div_sigma;
+    /** @brief complex-valued Fourier-stress on device */
+    cufftComplex *dev_sigma_hat;
+    /** @brief complex-valued divergence of Fourier-stress on device */
+    cufftComplex *dev_div_sigma_hat;
+    /** @brief complex-valued residual on device */
+    cufftComplex *dev_residual;
+    /** @brief temporary storage for zero mode of sigma in Fourier space */
+    cufftComplex *sigma_0;
+    /** @brief cuBLAS handle */
+    cublasHandle_t handle;
+    /** @brief cuFFT plan */
+    cufftHandle plan;
+};
 
 /** @brief Solve linear elasticity problem with Lippmann-Schwinger iteration
+ *
+ * Provides an interface which can be called externally
  *
  * @param[in] lambda Lame parameter lambda (host array, size ncells)
  * @param[in] mu Lame parameter mu (host array, size ncells)
