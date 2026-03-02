c2229724dd46d006c6e4587ad9e9b965b7e907d9
Eike Hermann Mueller <e.mueller@bath.ac.uk>
2026-03-02 13:31:54 +0000

    Added comments

--- cuda/src/common.hh
+++ cuda/src/common.hh
@@ -2,9 +2,9 @@
 #ifndef COMMON_HH
 #define COMMON_HH COMMON_HH
 
+#include "cufft.h"
 #include <math.h>
 #include <stdio.h>
-#include "cufft.h"
 
 // Block size used for 3d kernel launches
 #define BLOCKSIZE_X 8
@@ -20,43 +20,37 @@
 // Convert 3d index to linear index
 #define IDX(Nx, Ny, Nz, i, j, k) ((i) + (Nx) * ((j) + (Ny) * (k)))
 // Convert (1+3)d index to linear index
-#define FIDX(Nx, Ny, Nz, mu, i, j, k) \
+#define FIDX(Nx, Ny, Nz, mu, i, j, k)                                          \
   ((i) + (Nx) * ((j) + (Ny) * ((k) + (Nz) * (mu))))
 
 // Error checking, as defined in CUDA Programming guide, Section 2.1.7
-#define CUDA_CHECK(expr_to_check)                                      \
-  do                                                                   \
-  {                                                                    \
-    cudaError_t result = expr_to_check;                                \
-    if (result != cudaSuccess)                                         \
-    {                                                                  \
-      fprintf(stderr, "CUDA Runtime Error: %s:%i:%d = %s\n", __FILE__, \
-              __LINE__, result, cudaGetErrorString(result));           \
-    }                                                                  \
+#define CUDA_CHECK(expr_to_check)                                              \
+  do {                                                                         \
+    cudaError_t result = expr_to_check;                                        \
+    if (result != cudaSuccess) {                                               \
+      fprintf(stderr, "CUDA Runtime Error: %s:%i:%d = %s\n", __FILE__,         \
+              __LINE__, result, cudaGetErrorString(result));                   \
+    }                                                                          \
   } while (0)
 
 // Corresponding cuFFT error checking
-#define CUFFT_CHECK(expr_to_check)                                           \
-  do                                                                         \
-  {                                                                          \
-    cufftResult_t result = expr_to_check;                                    \
-    if (result != CUFFT_SUCCESS)                                             \
-    {                                                                        \
-      fprintf(stderr, "cuFFT Runtime Error: %s:%i:%d\n", __FILE__, __LINE__, \
-              result);                                                       \
-    }                                                                        \
+#define CUFFT_CHECK(expr_to_check)                                             \
+  do {                                                                         \
+    cufftResult_t result = expr_to_check;                                      \
+    if (result != CUFFT_SUCCESS) {                                             \
+      fprintf(stderr, "cuFFT Runtime Error: %s:%i:%d\n", __FILE__, __LINE__,   \
+              result);                                                         \
+    }                                                                          \
   } while (0)
 
 // Corresponding cuBLAS error checking
-#define CUBLAS_CHECK(expr_to_check)                                           \
-  do                                                                          \
-  {                                                                           \
-    cublasStatus_t stat = expr_to_check;                                      \
-    if (stat != CUBLAS_STATUS_SUCCESS)                                        \
-    {                                                                         \
-      fprintf(stderr, "cuBLAS Runtime Error: %s:%i:%d\n", __FILE__, __LINE__, \
-              stat);                                                          \
-    }                                                                         \
+#define CUBLAS_CHECK(expr_to_check)                                            \
+  do {                                                                         \
+    cublasStatus_t stat = expr_to_check;                                       \
+    if (stat != CUBLAS_STATUS_SUCCESS) {                                       \
+      fprintf(stderr, "cuBLAS Runtime Error: %s:%i:%d\n", __FILE__, __LINE__,  \
+              stat);                                                           \
+    }                                                                          \
   } while (0)
 
 /** @brief Specification of computational grid
@@ -64,8 +58,7 @@
  * Describes grid of the domain Lx x Ly x Lz with nx, ny, nz grid cells
  * (or voxels) in the different coordinate directions
  */
-struct GridSpec
-{
+struct GridSpec {
   int nx;   // Number of grid cells in x-direction
   int ny;   // Number of grid cells in y-direction
   int nz;   // Number of grid cells in z-direction
@@ -87,12 +80,76 @@ struct GridSpec
  */
 float relative_difference(float *u, float *u_ref, const int ndof);
 
-/* @brief Compute norm of vector field */
+/** @brief Compute norm of real-valued vector field
+ *
+ * returns the norm
+ *
+ *    sqrt( sum_{n=0}^{ncells-1} ||u_n||^2 )
+ *
+ * where
+ *
+ *     ||u_n|| = sqrt( sum_{j=0}^{2} u_{n,j}^2 )
+ *
+ * is the norm of the vector field u_j in a given cell with index j.
+ *
+ * @param[in] u vector field, host array of size 3*ncells
+ * @param[in] ncells number of cells
+ */
 float vector_norm(float *u, const int ncells);
+
+/** @brief Compute norm of complex-valued vector field
+ *
+ * returns the norm
+ *
+ *    sqrt( sum_{n=0}^{ncells-1} ||u_n||^2 )
+ *
+ * where
+ *
+ *     ||u_n|| = sqrt( sum_{j=0}^{2} |u_{n,j}|^2 )
+ *
+ * is the norm of the vector field u_j in a given cell with index j.
+ *
+ * @param[in] u vector field, host array of size 3*ncells
+ * @param[in] ncells number of cells
+ */
 float vector_norm(cufftComplex *u, const int ncells);
 
-/* @brief Compute norm of tensor field */
-float tensor_norm(float *u, const int ncells);
-float tensor_norm(cufftComplex *u, const int ncells);
+/** @brief Compute norm of real-valued tensor field in Voigt notation
+ *
+ * returns the norm
+ *
+ *    sqrt( sum_{n=0}^{ncells-1} ||tau_n||^2 )
+ *
+ * where
+ *
+ *     ||tau_j|| = sqrt( sum_{i!=j} tau_{n,ij}^2 )
+ *               = sqrt( tau_{n,0}^2 + tau_{n,1}^2 + tau_{n,2}^2
+ *                       + 2 (tau_{n,1}^2 + tau_{n,4}^2 + tau_{n,5}^2) )
+ *
+ * is the norm of the vector field tau_j in a given cell with index j.
+ *
+ * @param[in] u tensor field in Voigt notation, host array of size 6*ncells
+ * @param[in] ncells number of cells
+ */
+float tensor_norm(float *tau, const int ncells);
+
+/** @brief Compute norm of real-valued tensor field in Voigt notation
+ *
+ * returns the norm
+ *
+ *    sqrt( sum_{n=0}^{ncells-1} ||tau_n||^2 )
+ *
+ * where
+ *
+ *     ||tau_j|| = sqrt( sum_{i!=j} tau_{n,ij}^2 )
+ *               = sqrt( |tau_{n,0}|^2 + |tau_{n,1}|^2 + |tau_{n,2}|^2
+ *                       + 2 (|tau_{n,1}|^2 + |tau_{n,4}|^2 + |tau_{n,5}|^2) )
+ *
+ * is the norm of the vector field tau_j in a given cell with index j.
+ *
+ * @param[in] u tensor field in Voigt notation, host array of size 6*ncells
+ * @param[in] ncells number of cells
+ */
+float tensor_norm(cufftComplex *tau, const int ncells);
 
 #endif // COMMON_HH
